f1648e7ef39a032bda3787596dac29028005a2f1
Naharashu <130159705+Naharashu@users.noreply.github.com>
2026-06-26 14:16:53 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ const path = require("path");
 const app = express();
 const port = 8080;
 
+app.use(express.static("."));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static("."));
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
