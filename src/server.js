a4bdcf1463bbfbdccbe2520543d5c3ed59feaa15
Naharashu <130159705+Naharashu@users.noreply.github.com>
2026-06-26 14:18:48 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,6 +9,18 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/shop', (req, res) => {
+  res.sendFile(path.join(__dirname, 'shop.html'));
+});
+
+app.get('/settings', (req, res) => {
+  res.sendFile(path.join(__dirname, 'settings.html'));
+});
+
+app.get('/thank_to', (req, res) => {
+  res.sendFile(path.join(__dirname, 'thanks_to.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
